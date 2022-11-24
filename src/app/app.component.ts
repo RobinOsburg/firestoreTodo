42e5581c9f7cb1fe8bcdd81cfda2c814c8078cf6
Robin Osburg <Robin.osburg@web.de>
2022-11-24 11:09:35 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  todos$: Observable<any>;
+  constructor(firestore: Firestore) {
+    const coll = collection(firestore, 'todos');
+    this.todos$ = collectionData(coll);
+
+    this.todos$.subscribe( (newTodos) => {
+      console.log('neue Todos sind:',newTodos);
+    });
+  }
+
+
+}
